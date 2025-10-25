7dd7848e00bd5a678ab71aedfd560e0226007095
mengru-zhang-2357 <mzhang@makenacap.com>
2025-10-25 15:39:08 -0600

    Initial commit. Read data

---
+++ Group7_Assignment3_v1.R
@@ -0,0 +1,9 @@
+# Setting the working directory - Please change to your directory as you see fit
+setwd("~/Personal/Brown/BHDS2010/Assignment 3/Assignment3")
+
+library(readxl)
+
+text_msg <- read.csv("TextMessages.csv")
+
+head(text_msg)
+
